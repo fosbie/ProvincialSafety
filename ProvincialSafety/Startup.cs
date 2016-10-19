541edf2ebb0af634680475228c88fb6fe086d8b8
fosbie <bgfoster@gmail.com>
2016-10-19 16:31:35 +0100

    First checkin

---
+++ ProvincialSafety/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProvincialSafety.Startup))]
+namespace ProvincialSafety
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
