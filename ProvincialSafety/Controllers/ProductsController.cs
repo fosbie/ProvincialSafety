267f80163d1a79a8c18a1bf3aa75262be0fe4e2c
fosbie <bgfoster@gmail.com>
2016-10-31 16:49:32 +0000

    Updated logo and added HeadProtection view

---
+++ ProvincialSafety/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProvincialSafety.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult HeadProtection()
+        {
+            return View();
+        }
+
+        public ActionResult Clothing()
+        {
+            return View();
+        }
+
+        public ActionResult HandProtection()
+        {
+            return View();
+        }
+    }
+}
